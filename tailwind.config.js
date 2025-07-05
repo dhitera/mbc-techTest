5c9146598e608186ac7a8b0a772fcfb115eed4ad
myusername <mradithyanaufal@gmail.com>
2025-07-05 13:02:28 +0700

    Setup React and Tailwind with Flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import flowbite from "flowbite/plugin";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite],
+};
+
+export default config;
